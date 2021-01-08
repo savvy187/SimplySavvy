b2947fdeb3a936ace1a9d73b3ebd6c195009d3c5
savvy187 <savvy187@gmail.com>
2021-01-07 20:39:44 -0500

    better typography alignment

--- src/client/components/typography.component.js
+++ src/client/components/typography.component.js
@@ -4,45 +4,51 @@ const headingMixin = css`
     display: inline-block;
     text-transform: capitalize;
     cursor: pointer;
+    color: ${props => props.theme.colors.typography.headings.default};
 
     &:hover {
         text-decoration: underline;
         text-decoration-skip: objects;
     }
-`;
-const typographyMixin = css`        
-    color: ${props => props.theme.colors.typography.default};    
 
     &::selection {
-        color: ${({ theme }) => theme.colors.typography.selected};
+        color: ${({ theme }) => theme.colors.typography.headings.selected};
+        background-color: ${({ theme }) => theme.backgrounds.typography.headings.selected};
+        text-decoration: none;
+    }
+`;
+const typographyMixin = css`        
+    color: ${props => props.theme.colors.typography.p.default};    
+
+    &::selection {
+        color: ${({ theme }) => theme.colors.typography.p.selected};
+        background-color: ${({ theme }) => theme.backgrounds.typography.p.selected};
     }
 `;
 
 export const H1 = styled.h1`
     font: ${({ theme }) => theme.fonts.typography.h1};
-    ${headingMixin}
     ${typographyMixin}
+    ${headingMixin}
 `;
 
 export const H2 = styled.h2`
     font: ${({ theme }) => theme.fonts.typography.h2};
     ${headingMixin}
-    ${typographyMixin}
 `;
 
 export const H3 = styled.h3`
-    font: ${({ theme }) => theme.fonts.typography.h3};
+    font: ${({ theme }) => theme.fonts.typography.h3};    
     ${headingMixin}
-    ${typographyMixin}
 `;
 
 export const H4 = styled.h4`
-    font: ${({ theme }) => theme.fonts.typography.h4};
+    font: ${({ theme }) => theme.fonts.typography.h4};    
     ${headingMixin}
-    ${typographyMixin}
 `;
 
 export const P = styled.p`
     font: ${({ theme }) => theme.fonts.typography.p};
     ${typographyMixin}
+    line-height: 1.25;
 `;
